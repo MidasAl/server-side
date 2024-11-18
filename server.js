ff1d23610c55ea560070683f8c89d3439304df23
Jalen Xing <xing.jalen@gmail.com>
2024-11-17 22:23:13 -0800

    port change

--- server.js
+++ server.js
@@ -686,5 +686,5 @@ app.get("/api/users_reimbursements", isAuthenticated, async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 4999;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
